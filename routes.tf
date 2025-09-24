f437f01e637f06b0721f19ba8d607565fef2fbdd
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-09-24 10:43:16 -0300

    Create routes.tf

---
+++ routes.tf
@@ -0,0 +1,35 @@
+# RT público (una para todas las públicas)
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  tags   = merge(var.tags, { Name = "${var.name}-public-rt" })
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+# RT privadas (una por subnet privada)
+resource "aws_route_table" "private" {
+  for_each = aws_subnet.private
+  vpc_id   = aws_vpc.this.id
+  tags     = merge(var.tags, { Name = "${var.name}-private-rt-${each.value.availability_zone}" })
+}
+
+# Default route de privadas -> NAT (si está habilitado)
+resource "aws_route" "private_default_via_nat" {
+  for_each = var.enable_nat ? aws_route_table.private : {}
+
+  route_table_id         = each.value.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = var.single_nat_gateway
+    ? aws_nat_gateway.this[0].id
+    : aws_nat_gateway.per_az[tonumber(each.key)].id
+}
