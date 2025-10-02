39f5928ae28bd0426ecc2065543590c67d184bbd
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-10-02 15:37:54 -0300

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -4,8 +4,14 @@ data "aws_ami" "al2023" {
   most_recent = true
   owners      = ["amazon"]
 
-  filter { name = "name",         values = ["al2023-ami-*"] }
-  filter { name = "architecture", values = [local.bastion_arch] } # x86_64 o arm64
+  filter {
+    name = "name"
+    values = ["al2023-ami-*"]
+  }
+  filter {
+    name = "architecture"
+    values = [local.bastion_arch]
+  } # x86_64 o arm64
 }
 
 resource "aws_instance" "bastion" {
