7edb32129890cb7bd90045d69d215c9f83de33c3
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-10-01 22:27:26 -0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,8 +3,6 @@ resource "aws_vpc" "this" {
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags                 = merge(var.tags, { Name = "${var.name}-vpc" })
-  enable_dns_support   = true
-  enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "this" {
