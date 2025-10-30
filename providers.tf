e3ea3e24397641f83768cb567435cb06d9b5b79c
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-10-30 11:48:54 -0300

    Update role ARN to use aws_account_id variable

--- providers.tf
+++ providers.tf
@@ -12,6 +12,6 @@ provider "aws" {
   region = var.aws_region
 
   assume_role {
-    role_arn = "arn:aws:iam::${var.account_id}:role/TerraformDeploymentRole-ecolors-nonprod"
+    role_arn = "arn:aws:iam::${var.aws_account_id}:role/TerraformDeploymentRole-ecolors-nonprod"
   }
 }
