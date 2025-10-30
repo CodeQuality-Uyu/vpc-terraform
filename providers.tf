8cbde365f04c8e38705606585e817282b2cce5fb
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-10-30 12:46:36 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,8 +10,4 @@ terraform {
 
 provider "aws" {
   region = var.aws_region
-
-  assume_role {
-    role_arn = "arn:aws:iam::${var.aws_account_id}:role/TerraformDeploymentRole-ecolors-nonprod"
-  }
 }
